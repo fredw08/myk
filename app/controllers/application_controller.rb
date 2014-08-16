13d41f86bd1944ac365ecd1cfd3f5fcb72555bcc
fredw08 <fredw08@gmail.com>
2014-08-16 23:51:39 +0800

    relocate collection_paginate from Base to Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,30 @@ class ApplicationController < ActionController::Base
     method   = "#{resource}_params"
     params[resource] &&= send(method) if respond_to?(method, true)
   end
+
+  def collection_paginate(instance_name: collection_instance_name, listing_scope: :index_listing, scope_param: false)
+    per_page = params[:per_page] && params[:per_page].to_i > 0 ? params[:per_page].to_i : PER_PAGE
+    page     = params[:page]     && params[:page].to_i > 0     ? params[:page].to_i     : 1
+
+    obj = instance_variable_get(instance_name)
+
+    params[:search].each do |k, v|
+      k = "search_#{k}"
+      obj = obj.send(k, v) if obj.respond_to?(k)
+    end if params[:search] && params[:search].is_a?(Hash)
+
+    if obj.respond_to?(listing_scope)
+      if scope_param
+        obj = obj.send(listing_scope, *scope_param)
+      else
+        obj = obj.send(listing_scope)
+      end
+    end
+
+    # response.headers['total'] = "#{obj.count}"
+    instance_variable_set(
+      instance_name,
+      obj.paginate(page: page, per_page: per_page).order(sort_column + " " + sort_direction)
+    )
+  end
 end
